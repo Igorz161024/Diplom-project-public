3c75d0ec28d2d6c8220821d4d749528c396e46be
Igorz161024 <igorz2005@gmail.com>
2026-03-27 12:50:03 +0200

    fix: пробний запуск CI з оновленими файлами

--- git-save.ps1
+++ git-save.ps1
@@ -1,7 +1,10 @@
+[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
+
 git add .
 git commit -m "Автоматичний коміт" --allow-empty
 git push private master
 git push public master
 git push both master
+
 Write-Output "Зміни збережено на GitHub (приватний та публічний репозиторії)"
 git status

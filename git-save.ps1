f51db60f409270f936169ba636ec57af1db47b10
Igorz161024 <igorz2005@gmail.com>
2026-03-28 19:37:02 +0200

    РђРІС‚РѕРјР°С‚РёС‡РЅРёР№ РєРѕРјС–С‚

--- git-save.ps1
+++ git-save.ps1
@@ -1,10 +1,16 @@
 [Console]::OutputEncoding = [System.Text.Encoding]::UTF8
 
+# Додаємо всі зміни
 git add .
+
+# Коміт з повідомленням (порожній коміт дозволений)
 git commit -m "Автоматичний коміт" --allow-empty
-git push private master
-git push public master
-git push both master
+
+# Пушимо у приватний репозиторій, гілка Владелец
+git push private Владелец
+
+# Пушимо у публічний репозиторій, гілка Владелец
+git push public Владелец
 
 Write-Output "Зміни збережено на GitHub (приватний та публічний репозиторії)"
 git status

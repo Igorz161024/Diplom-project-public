56759a25a377203254571b77193f70196827e813
Igorz161024 <igorz2005@gmail.com>
2026-04-03 16:01:56 +0300

    РђРІС‚РѕРјР°С‚РёС‡РЅРёР№ РєРѕРјС–С‚

--- git-save.ps1
+++ git-save.ps1
@@ -1,20 +1,11 @@
 [Console]::OutputEncoding = [System.Text.Encoding]::UTF8
-git config --global i18n.commitEncoding utf-8
-git config --global i18n.logOutputEncoding utf-8
+chcp 65001 > $null
 
-# Додаємо всі зміни
 git add .
-
-# Коміт з повідомленням (порожній коміт дозволений)
 git commit -m "Автоматичний коміт" --allow-empty
 
-# Пушимо у приватний репозиторій, гілка Владелец
-git push private Владелец
-
-# Пушимо у публічний репозиторій, гілка Владелец
-git push public Владелец
+git push private owner
+git push public owner
 
 Write-Output "Зміни збережено на GitHub (приватний та публічний репозиторії)"
 git status
-
-

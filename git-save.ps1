c3a9a1fa57b40d3f0788de361c7be094e5752f10
Igorz161024 <igorz2005@gmail.com>
2026-04-03 15:52:33 +0300

    РђРІС‚РѕРјР°С‚РёС‡РЅРёР№ РєРѕРјС–С‚

--- git-save.ps1
+++ git-save.ps1
@@ -1,4 +1,6 @@
 [Console]::OutputEncoding = [System.Text.Encoding]::UTF8
+git config --global i18n.commitEncoding utf-8
+git config --global i18n.logOutputEncoding utf-8
 
 # Додаємо всі зміни
 git add .

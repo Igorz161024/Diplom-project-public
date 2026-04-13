58b3a12be2c331f4119f25c143a9796e3a6fac32
Igorz161024 <igorz2005@gmail.com>
2026-04-13 14:12:20 +0300

    РђРІС‚РѕРјР°С‚РёС‡РЅРёР№ РєРѕРјС–С‚

--- git-save.ps1
+++ git-save.ps1
@@ -1,9 +1,12 @@
 [Console]::OutputEncoding = [System.Text.Encoding]::UTF8
 chcp 65001 > $null
 
+param([string]$message = "Автоматичний коміт")
+
 git add .
-git commit -m "Автоматичний коміт"
+git commit -m $message
 git push origin master
 
 Write-Output "Зміни збережено на GitHub (гілка master)"
 git status
+
